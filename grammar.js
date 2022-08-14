12012f17051bafbd0ee9f06597f535c93ade1ff5
Lennard Hofmann <lennard.hofmann@web.de>
2022-08-14 15:23:16 +0200

    Add support for HTML, wikilinks, templates, and templates

--- grammar.js
+++ grammar.js
@@ -1,26 +1,91 @@
-module.exports = grammar({
+const HTML = require('tree-sitter-html/grammar');
+
+module.exports = grammar(HTML, {
   name: 'mediawiki',
-  extras: $ => [], // whitespace is significant
+
   rules: {
-    source_file: $ => repeat($._root),
-    
-    _root: $ => choice(
-      $.wikilink,
-      $._plaintext, // let's hope this acts as a fallback if nothing else matches
+    _node: $ => choice(
+      $._inlinemarkup,
+      $._blockmarkup
     ),
     
+    _inlinemarkup: $ => choice(
+      $.extlink,
+      $.wikilink,
+      $.template,
+      $.parameter,
+      $.element,
+      $.erroneous_end_tag,
+      /.|\n/ // plain text
+    ),
+    
+    _blockmarkup: $ => choice(
+      $.heading,
+    ),
+
+    extlink: $ => seq(
+      '[',
+      field('target', $.url),
+      optional(seq(
+        ' ',
+        optional(field('label', $.extlinklabel))
+      )),
+      ']'
+    ),
+
     wikilink: $ => seq(
       '[[',
       field('target', $.linktarget),
       optional(
         field('anchor', $.linkanchor)
       ),
-      /(\|.*?)?\]\]/ // two "]" optionally preceeded by "|linktext"
+      optional(seq(
+        '|',
+        optional(field('label', $.linklabel))
+      )),
+      ']]'
+    ),
+
+    template: $ => seq(
+      '{{',
+      field('name', $.templatename),
+      repeat(seq('|', $.templateparam)),
+      '}}'
+    ),
+
+    // parameter in a template, e.g. {{{name|default}}}
+    parameter: $ => seq(
+      '{{{',
+      field('name', $.paramname),
+      optional(seq('|', optional($._paramdefault))),
+      '}}}'
     ),
     
-    linktarget: $ => /[^#|\]]+/, // everything until first "#" or "]" char
-    linkanchor: $ => /#[^|\]]+/, // this assumes the anchor contains no "]"
+    heading: $ => choice( $.h1, $.h2, $.h3, $.h4, $.h5, $.h6 ),
+    h1: $ => seq('\n=',      /[^=\n]+/, '='),
+    h2: $ => seq('\n==',     /[^=\n]+/, '=='),
+    h3: $ => seq('\n===',    /[^=\n]+/, '==='),
+    h4: $ => seq('\n====',   /[^=\n]+/, '===='),
+    h5: $ => seq('\n=====',  /[^=\n]+/, '====='),
+    h6: $ => seq('\n======', /[^=\n]+/, '======'),
+    
+    url: $ => seq(
+      /([a-zA-Z0-9.-]+:|\/\/)/, // URI scheme or protocol relative
+      /[^\s\]]+/                // all except whitespace and "]"
+    ),
+    extlinklabel: $ => /[^\s\]]+/, // all except whitespace and "]"
 
-    _plaintext: $ => /.|\n/
+    linktarget: $ => /[^#|\]]+/, // everything until first "#", "|", or "]" char
+    linkanchor: $ => /#[^|\]]+/, // this assumes the anchor contains no "]"
+    linklabel:  $ => repeat1(/[^\]]|\][^\]]/),
+    
+    templatename: $ => /[^|}]+/, // everything until first "|" or "}" char
+    templateparam: $ => seq(
+      optional(field('key', /[^|}]+?=/)),
+      field('value', repeat1($._node))
+    ),
+
+    paramname: $ => /[^|}]+/, // everything until first "|" or "}" char
+    _paramdefault: $ => field('value', repeat1($._node))
   }
 });
